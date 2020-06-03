aaffe01112e9e52847eb38f8a69b7cb14eaf8ebe
Rebirator <terretoriatitanx@gmail.com>
2020-06-03 14:02:02 +0300

    Теперь просто вводим нужное время добычи.

--- Game/Woodcutter Simulator.cpp
+++ Game/Woodcutter Simulator.cpp
@@ -21,49 +21,21 @@ int main()
 
     int firewood_variant;
 
-// START
-start:
+//  START
+    start:
     cout << "You have " << wood << " wood." << endl << endl;;
 
     cout << "What to do?" << endl
          << "1 = Chopping wood(" << wood_in_second << " per second)." << endl;
-//        << "2 = Earn firewood by solving tasks" << endl;
+//         << "2 = Earn firewood by solving tasks" << endl;
     cin >> firewood_variant;
     cout << endl;
 
     if (firewood_variant == 1)
     {
-        cout << "How long does it take to chop wood?" << endl
-            << "0 = 1 Second." << endl
-            << "1 = 10 Seconds." << endl
-            << "2 = 1 Minute." << endl
-            << "3 = Hour." << endl
-            << "4 = Day." << endl;
-        cin >> time_variant;
+        cout << "How long does it take to chop wood?(in seconds): " << endl;
+        cin >> time;
         cout << endl;
-
-        switch (time_variant)
-        {
-        case 0:
-            time = second;
-            break;
-        case 1:
-            time = second * 10;
-            break;
-        case 2:
-            time = minute;
-            break;
-        case 3:
-            time = hour / 10;
-            break;
-        case 4:
-            time = day / 24 / 3;
-            break;
-        default:
-            cout << "You entered the wrong option, return to the beginning.\n\n" << endl;
-//          YES, I WILL USE IT!
-            goto start;
-        }
 
         for (size_t i = 0; i < time; i++)
         {
@@ -73,7 +45,6 @@ start:
         }
         cout << endl;
 
-//      YES, I WILL USE IT!
         goto start;
     }
 }
